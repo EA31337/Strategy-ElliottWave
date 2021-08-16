e4d26ccc6b42a73cd93fb080a7d5c5fad8df971c
kenorb <kenorb@users.noreply.github.com>
2021-08-16 23:14:13 +0100

    Disables __resource__ by default

--- config/define.h
+++ config/define.h
@@ -9,4 +9,4 @@
 //#define __debug__        // Enables debugging.
 #define __input__  // Enables input parameters.
 //#define __optimize__     // Enables optimization mode.
-#define __resource__  // Enables resources.
+//#define __resource__  // Enables resources.
